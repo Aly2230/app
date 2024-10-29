3f95d33fe71fe0a85edc50a683f7b6f6c13729ed
Aly2230 <156617627+Aly2230@users.noreply.github.com>
2024-10-29 16:20:58 -0300

    Update secret.py

--- app/secret.py
+++ app/secret.py
@@ -1,5 +1,5 @@
 import secrets
-#o IDEIAL SERIA UMA FERRMANTA PARA GERAR ESSA CHAVE E DENTRO DE UMA VARIÁVEL DE AMBIENTE E NAO GERAR ASSIM
+#o IDEIAL SERIA UMA FERRAMENTA PARA GERAR ESSA CHAVE E DENTRO DE UMA VARIÁVEL DE AMBIENTE E NAO GERAR ASSIM
 #crie um arquivo .py como nome secret para gerar,importe serets que é uma bibioteca já do py instacie  a variável sk=secrets.token_hex(24)o o n° é a quantidade de caracters printa e coloca a chave #
 sk=secrets.token_hex(24)
 print(sk)
